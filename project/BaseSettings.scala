2841687009de4d3796867c890c00f504e6909cd7
Krisztian Lachata <krisztian.lachata@ovoenergy.com>
2018-10-28 11:22:37 +0000

    fix service name

--- project/BaseSettings.scala
+++ project/BaseSettings.scala
@@ -1,5 +1,5 @@
-import sbt.{ Def, _ }
 import sbt.Keys._
+import sbt.{Def, _}
 
 object BaseSettings {
 
@@ -8,8 +8,8 @@ object BaseSettings {
       organization := "org.qualiton",
       organizationName := "Qualiton Ltd",
       organizationHomepage := Some(url("https://www.qualiton.org/")),
-      name := "Git Discussions Crawler",
-      moduleName := "git-discussions-crawler",
+      name := "Github Discussions Crawler",
+      moduleName := "github-discussions-crawler",
       description := "Collects git discussion info and publishes on Slack",
       scalaVersion := "2.12.7",
       shellPrompt := { s => "[" + scala.Console.BLUE + Project.extract(s).currentProject.id + scala.Console.RESET + "] $ " }
