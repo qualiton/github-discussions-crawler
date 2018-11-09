8a5cae54858ed9970197a3fabf6a5ec6e5cc92ed
Krisztian Lachata <krisztian.lachata@ovoenergy.com>
2018-11-09 23:22:56 +0000

    add github api mock

--- crawler/src/test/scala/org/qualiton/crawler/testsupport/wiremock/GithubApiV3MockServer.scala
+++ crawler/src/test/scala/org/qualiton/crawler/testsupport/wiremock/GithubApiV3MockServer.scala
@@ -1,10 +1,21 @@
 package org.qualiton.crawler.testsupport.wiremock
 
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.concurrent.atomic.AtomicLong
+
+import cats.instances.either._
+import cats.instances.list._
+import cats.syntax.traverse._
+
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
-import com.github.tomakehurst.wiremock.http.HttpStatus
+import com.github.tomakehurst.wiremock.http.{ HttpStatus => HttpStatusCheck }
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import io.circe.parser._
+import io.circe.Json
+import org.apache.http.HttpStatus
 
 import org.qualiton.crawler.testsupport.resource.ResourceSupport
 import org.qualiton.crawler.testsupport.wiremock.GithubApiV3MockServer._
@@ -31,12 +42,14 @@ class GithubApiV3MockServer(githubApiV3Port: Int = GithubApiV3Port) extends Reso
     """.stripMargin
 
   private def prepareResponse(resource: String, httpStatus: Int): String = {
-    if (HttpStatus.isSuccess(httpStatus)) {
+    if (HttpStatusCheck.isSuccess(httpStatus)) {
       loadResource(resource)
     } else {
       errorResponse
     }
   }
+
+  private val fixedInstant = Instant.parse("2018-11-10T08:09:45Z")
 
   def mockUserTeams(teamId: Long, httpStatus: Int, resource: String = "/userteams.json"): StubMapping =
     server.stubFor(get(urlPathMatching("/user/teams"))
@@ -47,24 +60,80 @@ class GithubApiV3MockServer(githubApiV3Port: Int = GithubApiV3Port) extends Reso
         .withBody(prepareResponse(resource, httpStatus)
           .replaceAll("TEAM_ID", teamId.toString))))
 
-  def mockTeamDiscussions(teamId: Long, httpStatus: Int, resource: String = "/teamdiscussions-1.json"): StubMapping =
+  def mockTeamDiscussions(teamId: Long, numberOfDiscussion: Int, numberOfComments: Int, httpStatus: Int, resource: String = "/teamDiscussionsItem.json"): StubMapping = {
+    val body: String = {
+      val counter = new AtomicLong(1)
+
+      def discussionArrayItem = {
+        val current = counter.getAndIncrement()
+        mockDiscussionComments(teamId, current, numberOfComments, httpStatus)
+        parse(loadResource(resource)
+          .replaceAll("TEAM_ID", teamId.toString)
+          .replaceAll("BODY", s"discussion-body-$current")
+          .replaceAll("COMMENTS", numberOfComments.toString)
+          .replaceAll("DISCUSSION_ID", current.toString)
+          .replaceAll("CREATED_AT", fixedInstant.plus(current, ChronoUnit.HOURS).toString)
+          .replaceAll("NUMBER", current.toString)
+          .replaceAll("TITLE", s"discussion-title-$current"))
+      }
+
+      Json.fromValues(
+        Stream
+          .continually(discussionArrayItem)
+          .take(numberOfDiscussion)
+          .toList
+          .reverse
+          .sequence
+          .getOrElse(List.empty))
+        .toString()
+    }
+
     server.stubFor(get(urlPathMatching(s"/teams/$teamId/discussions"))
       .withHeader("Authorization", equalTo(s"Basic $testEncodedApiToken"))
       .willReturn(aResponse()
         .withStatus(httpStatus)
         .withHeader("Content-type", "application/json; charset=UTF-8")
-        .withBody(prepareResponse(resource, httpStatus)
-          .replaceAll("TEAM_ID", teamId.toString))))
+        .withBody(body)))
+  }
 
-  def mockDiscussionComments(teamId: Long, discussionId: Long, httpStatus: Int, resource: String = "/discussioncomments-1.json"): StubMapping =
+  def mockDiscussionComments(teamId: Long, discussionId: Long, numberOfComments: Int, httpStatus: Int, resource: String = "/discussionCommentsItem.json"): StubMapping = {
+    val body: String = {
+      val counter = new AtomicLong(1)
+
+      def commentArrayItem = {
+        val current = counter.getAndIncrement()
+        parse(loadResource(resource)
+          .replaceAll("TEAM_ID", teamId.toString)
+          .replaceAll("BODY", s"comment-body-$current")
+          .replaceAll("DISCUSSION_ID", discussionId.toString)
+          .replaceAll("COMMENT_ID", current.toString)
+          .replaceAll("CREATED_AT", fixedInstant.plus(current, ChronoUnit.HOURS).toString)
+          .replaceAll("NUMBER", current.toString))
+      }
+
+      Json.fromValues(
+        Stream
+          .continually(commentArrayItem)
+          .take(numberOfComments)
+          .toList
+          .reverse
+          .sequence
+          .getOrElse(List.empty))
+        .toString()
+    }
+
     server.stubFor(get(urlPathMatching(s"/teams/$teamId/discussions/$discussionId/comments"))
       .withHeader("Authorization", equalTo(s"Basic $testEncodedApiToken"))
       .willReturn(aResponse()
         .withStatus(httpStatus)
         .withHeader("Content-type", "application/json; charset=UTF-8")
-        .withBody(prepareResponse(resource, httpStatus)
-          .replaceAll("TEAM_ID", teamId.toString)
-          .replaceAll("DISCUSSION_ID", discussionId.toString))))
+        .withBody(body)))
+  }
+
+  def mockDiscussions(teamId: Long, numberOfDiscussion: Int, numberOfComments: Int, httpStatus: Int): StubMapping = {
+    mockUserTeams(teamId, HttpStatus.SC_OK)
+    mockTeamDiscussions(teamId, numberOfDiscussion, numberOfComments, httpStatus)
+  }
 }
 
 object GithubApiV3MockServer {
--- server/src/e2e/scala/org/qualiton/crawler/CrawlerEndToEndSpec.scala
+++ server/src/e2e/scala/org/qualiton/crawler/CrawlerEndToEndSpec.scala
@@ -2,9 +2,12 @@ package org.qualiton.crawler
 
 import cats.scalatest.{ EitherMatchers, ValidatedValues }
 
+import org.apache.http.HttpStatus
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.{ Matchers, _ }
 import org.scalatest.concurrent.Eventually
+
+import org.qualiton.crawler.testsupport.scalatest.GithubApiV3MockServerSupport
 
 class CrawlerEndToEndSpec
   extends FeatureSpec
@@ -16,12 +19,15 @@ class CrawlerEndToEndSpec
     with ValidatedValues
     with Eventually
     with TypeCheckedTripleEquals
+    with GithubApiV3MockServerSupport
     with Inside {
 
   feature("Internal status endpoint") {
     scenario("Status should return successfully") {
       When("Hitting the status endpoint")
       Then("we should receive empty body")
+
+      mockGithubApiV3MockServer.mockDiscussions(1, 1, 2, HttpStatus.SC_OK)
     }
   }
 }
